989f6fd7e92a24e31a49ce679099bceb87d0b2ba
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2014-09-05 09:03:03 -0600

    refactor to add some extension points

--- src/main/scala/latis/reader/tsml/NetcdfAdapter.scala
+++ src/main/scala/latis/reader/tsml/NetcdfAdapter.scala
@@ -15,15 +15,29 @@ import ucar.nc2.util.EscapeStrings
  */
 class NetcdfAdapter(tsml: Tsml) extends TsmlAdapter(tsml) {
 
-  private var ncFile: NetcdfFile = null
+  //TODO: handle first, last, and limit filter and selections?
   
+  private var ncFile: NetcdfFile = null
+
+  def readData(vname: String, section: String = ""): ucar.ma2.Array = {
+    //Some names contain "." which findVariable will interpret as a structure member
+    //NetCDF library dropped NetcdfFile.escapeName between 4.2 and 4.3 so replicate with what it used to do.
+    //TODO: replace with "_"?
+    val escapedName = EscapeStrings.backslashEscape(vname, ".")
+    //val vname = vname.replaceAll("""\.""", """\\.""")
+    val ncvar = ncFile.findVariable(escapedName)
+
+    if (section == "") ncvar.read
+    else ncvar.read(section).reduce //drop extra dimensions
+  }
+
   override def init {
     //open the NetCDF file as defined by the location in the tsml.
     val location = getUrl.toString
     ncFile = NetcdfDataset.openFile(location, null)
-    
+
     //TODO: capture attributes in metadata
-    
+
     //For each variable defined in the tsml, read all the data from the 
     //NetCDF file and put it in the cache.
     for (v <- getOrigScalars) {
@@ -32,37 +46,32 @@ class NetcdfAdapter(tsml: Tsml) extends TsmlAdapter(tsml) {
         case Some(s) => s
         case None => v.getName
       }
-        
-      //Some names contain "." which findVariable will interpret as a structure member
-      //NetCDF library dropped NetcdfFile.escapeName between 4.2 and 4.3 so replicate with what it used to do.
-      //TODO: replace with "_"?
-      val escapedName = EscapeStrings.backslashEscape(vname, ".") 
-      //val vname = vname.replaceAll("""\.""", """\\.""")
-      val ncvar = ncFile.findVariable(escapedName)
-      
+
+      val section = v.getMetadata("section") match {
+        case Some(s) => s
+        case None => ""
+      }
+
       //Get data Array from Variable.
       //Apply optional 'section' property.
-      val ncarray = v.getMetadata("section") match {
-        case Some(s) => ncvar.read(s).reduce //drop extra dimensions
-        case None    => ncvar.read
-      }
-      
+      val ncarray = readData(vname, section)
+
       val n = ncarray.getSize.toInt //TODO: limiting length to int
       //val ds = (0 until n).map(ncarray.getObject(_)).map(Data(_)) //Let Data figure out how to store it, assuming primitive type
 
       //Store data based on the type of varible as defined in the tsml
       val datas = v match {
         case i: Integer => (0 until n).map(ncarray.getLong(_)).map(Data(_))
-        case r: Real    => (0 until n).map(ncarray.getDouble(_)).map(Data(_))
-        case t: Text    => (0 until n).map(ncarray.getObject(_)).map(o => Data(o.toString))
+        case r: Real => (0 until n).map(ncarray.getDouble(_)).map(Data(_))
+        case t: Text => (0 until n).map(ncarray.getObject(_)).map(o => Data(o.toString))
       }
-      
+
       val data = DataSeq(datas)
-      
+
       cache(v.getName, data)
     }
   }
-  
+
   def close = ncFile.close
 }
 
